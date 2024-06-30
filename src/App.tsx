8ecef84135b2ad2838c31763abd2651e4bbe7852
vinicius <vinicius.belucci@outlook.com>
2024-06-30 17:19:56 -0300

    Context-Home-Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import CreateOtherDiv from './CreateOtherDiv'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './context/AuthContext';
+
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
